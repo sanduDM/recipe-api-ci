263b0c121c9d1628bd6fba9ff2c4997ca9c3bd23
sanduDM <sanduni-im23123@stu.kln.ac.lk>
2026-08-03 23:04:51 +0530

    test: intentionally break api test

--- tests/app.test.js
+++ tests/app.test.js
@@ -10,9 +10,7 @@ describe("GET / API Test", () => {
 
         expect(response.statusCode).toBe(200);
 
-        expect(response.body.message)
-            .toBe("CI Pipeline Working");
-
+expect(response.body.message).toBe("CI Pipeline Broken");
     });
 
 });
